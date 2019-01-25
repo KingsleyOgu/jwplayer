94935d51fea12be58dcfc6656b50f1e3d9802b78
DanFerrer <danielferrer212@gmail.com>
2019-01-25 16:40:49 -0500

    make player float to top on mobile

--- src/js/view/view.js
+++ src/js/view/view.js
@@ -872,6 +872,7 @@ function View(_api, _model) {
             _model.set('floating', true);
 
             if (!_isMobile) {
+                addClass(_wrapperElement, 'jw-float-bottom');
                 _resizeOnFloat = true;
 
                 // Resize within MAX_FLOATING_WIDTH×MAX_FLOATING_HEIGHT bounds, never enlarge.
@@ -882,6 +883,8 @@ function View(_api, _model) {
                 _this.resize(floatingWidth, floatingHeight, true);
 
                 _resizeOnFloat = false;
+            } else {
+                addClass(_wrapperElement, 'jw-float-top');
             }
         } else if (isVisible) {
             _this.stopFloating();
